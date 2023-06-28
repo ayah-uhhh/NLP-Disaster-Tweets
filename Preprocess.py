a081eb798797d0c7073771fb51c5571ea17d4857
Moe580 <68943716+Moe580@users.noreply.github.com>
2023-06-28 22:09:30 +0000

    cleaned up some lines

--- Preprocess.py
+++ Preprocess.py
@@ -2,35 +2,16 @@
 # coding: utf-8
 
 # # Import the Dataset
-
-# In[ ]:
-
-
 import pandas as pd
-
-
-# In[15]:
-
 
 df_train = pd.read_csv('dataset/train.csv')
 df_test = pd.read_csv('dataset/test.csv')
 #sample_submission = pd.read_csv('dataset/sample_submission.csv')
 
 
-# In[20]:
-
-
-df_train.head()
-
-
 # # Cleaning the Text
-# 
 
 # Convert location and text to Lowercase for train and test dataset
-
-# In[25]:
-
-
 #TRAIN
 df_train['location'] = df_train['location'].str.lower()
 df_train['text'] = df_train['text'].str.lower()
@@ -40,16 +21,7 @@ df_test['location'] = df_test['location'].str.lower()
 df_test['text'] = df_test['text'].str.lower()
 
 
-# In[26]:
-
-
-df_test.head()
-
-
 # Remove URL's from text column for train and test dataset
-
-# In[35]:
-
 #debug
 #print('train before = \n',df_train.head(20))
 #print('test before = \n',df_test.head(20))
@@ -83,15 +55,5 @@ clean_data(df_train)
 clean_data(df_test)
 
 
-# In[38]:
-
-
 df_test.head(35)
 #print(df_train.iloc[33])
-
-
-# In[ ]:
-
-
-
-
