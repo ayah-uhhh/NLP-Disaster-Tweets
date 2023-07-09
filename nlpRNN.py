65a769039639f1fd00aff5a20e90ef181b9f682e
ayah <ayah.ayad333@gmail.com>
2023-07-09 15:16:14 -0500

    she doesn't like our data

--- nlpRNN.py
+++ nlpRNN.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import keras
 from keras import layers
-
+import time
+from sklearn.model_selection import train_test_split
 
 #Understand RNN performance
 def plot_graphs(history, metric):
@@ -14,21 +15,71 @@ def plot_graphs(history, metric):
     plt.ylabel(metric)
     plt.legend([metric, 'val_'+metric])
 
-
 #Import Data
-test_data = pd.read_csv('dataset/cleaned_test.csv')
-train_data = pd.read_csv('dataset/cleaned_train.csv')
+# test_data = pd.read_csv('dataset/cleaned_test.csv')
+# train_data = pd.read_csv('dataset/cleaned_train.csv')
 
-#bidirectional RNN: this allows context to be gained before and after words
+def nlp_rnn(optimizer='rmsprop', units=64, input_shape=(5, 10), show_chart=False, save=False, epochs=150, batch_size=32):
+    """Import Data"""
+    start_time = time.time()
+    train_data = pd.read_csv('dataset/cleaned_train.csv')
+    
+    # Extract X and Y from train_data
+    X = train_data[['keyword', 'location', 'text']].values
+    Y = train_data['target'].values
 
-model = keras.Sequential()
+    print("Splitting data...")
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, Y, test_size=0.2, shuffle=False)
 
-model.add(
-    layers.Bidirectional(layers.LSTM(64, return_sequences=True), input_shape=(5, 10))
-)
-model.add(layers.Bidirectional(layers.LSTM(32)))
-model.add(layers.Dense(10))
+    print("Building model...")
+    start_time = time.time()
+    # Define the model
+    model = keras.Sequential()
 
-model.summary()
+    # Bidirectional LSTM layers
+    model.add(layers.Bidirectional(layers.LSTM(units, return_sequences=True), input_shape=input_shape))
+    model.add(layers.Bidirectional(layers.LSTM(units)))
+    model.add(layers.Dense(7, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                  optimizer=optimizer, metrics=['accuracy'])
+
+    print("Built model. (%.2f)", time.time()-start_time)
+
+    model.summary()
+    # Train the model
+    print("Training model...")
+    history = model.fit(X_train, y_train, epochs, batch_size,
+                        validation_data=(X_test, y_test))
+    print("Model trained. (%.2f)", time.time()-start_time)
+
+    elapsed_time = time.time()-start_time
+
+    if (show_chart):
+        plt.plot(history.history['accuracy'], label='accuracy')
+        plt.plot(history.history['val_accuracy'], label='val_accuracy')
+        plt.xlabel('Epoch')
+        plt.ylabel('Accuracy')
+        plt.ylim([0.5, 1])
+        plt.legend(loc='lower right')
+        plt.show()
+
+    # Evaluate the model
+    loss, accuracy = model.evaluate(X_test, y_test, verbose=0)
+
+    if save:
+        print("Saving model...")
+        start_time = time.time()
+        model.save('ps_rnn_model.h5')
+        print("Saved model (%.2fs)", time.time()-start_time)
+
+    print('Accuracy: %.2f', (accuracy*100))
+    print("Loss: %s", loss)
+
+    return [(optimizer, units, input_shape), elapsed_time, loss, accuracy, model]
 
 
+nlp_rnn()
