bc36d45ab8e5f40db76dd1ab3d38126ae3fe2e14
Moe580 <moe.suemura@colorado.edu>
2023-07-16 13:45:14 -0600

    Added tokenization and lemmatization

--- Preprocess.py
+++ Preprocess.py
@@ -5,21 +5,17 @@ import pandas as pd
 
 import re
 
+import nltk
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+
 df_train = pd.read_csv('dataset/train.csv')
 df_test = pd.read_csv('dataset/test.csv')
 #sample_submission = pd.read_csv('dataset/sample_submission.csv')
 
 
 # # Cleaning the Text
-
-# Convert location and text to Lowercase for train and test dataset
-#TRAIN
-df_train['location'] = df_train['location'].str.lower()
-df_train['text'] = df_train['text'].str.lower()
-
-#TEST
-df_test['location'] = df_test['location'].str.lower()
-df_test['text'] = df_test['text'].str.lower()
 
 def clean_data(dataframe):
     # Convert location and text to lowercase
@@ -29,14 +25,23 @@ def clean_data(dataframe):
     # Remove URLs, numbers, and non-alphanumeric characters
     dataframe['text'] = dataframe['text'].apply(lambda x: re.sub(r'http[s]?://\S+|[^A-Za-z ]|\d+', ' ', str(x)))
     dataframe['location'] = dataframe['location'].apply(lambda x: re.sub(r'http[s]?://\S+|[^A-Za-z ]|\d+', ' ', str(x)))
+                                                
+    # Tokenization
+    dataframe['text'] = dataframe['text'].apply(lambda x: word_tokenize(str(x)))
 
-    # Load stopwords into a set
-    stopwords = set(pd.read_csv('dataset/NLTKs_list_of_english_stopwords', sep=' ', header=None, names=['stopwords'])['stopwords'])
-    
     # Remove stopwords
-    dataframe['text'] = dataframe['text'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
-    dataframe['location'] = dataframe['location'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
+    stopwords=nltk.corpus.stopwords.words('english')
+    dataframe['text'] = dataframe['text'].apply(lambda x: ' '.join([word for word in x if word not in stopwords])) # dataframe['location'] = dataframe['location'].apply(lambda x: ' '.join([word for word in x.split() if word not in stopwords]))
     
+    # Lemmatization (normalization)
+    lemmatizer=WordNetLemmatizer()
+    dataframe['text'] = dataframe['text'].apply(lambda x: lemmatizer.lemmatize(str(x)))
+    
+    # Remove nan
+    dataframe['location'] = dataframe['text'].apply(lambda x: re.sub(r'nan',',', str(x)))
+
+    
+
 # Clean the text
 clean_data(df_train)
 clean_data(df_test)
